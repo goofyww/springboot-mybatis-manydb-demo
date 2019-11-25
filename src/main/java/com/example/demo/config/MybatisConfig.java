6b206eb5cc98fb3716622e65c9504d71d0c6f0b9
root <root@localhost>
2019-11-25 13:40:51 +0800

    加入 spring environment

--- src/main/java/com/example/demo/config/MybatisConfig.java
+++ src/main/java/com/example/demo/config/MybatisConfig.java
@@ -4,9 +4,9 @@ import com.example.demo.comm.DataSources;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
@@ -25,6 +25,9 @@ public class MybatisConfig {
     @Qualifier(DataSources.SECONDARY)
     private DataSource secondary;
 
+    @Autowired
+    private Environment env;
+
     @Bean(name = "dynamicDataSource")
     public DataSource dynamicDataSource() {
         DynamicDataSource dynamicDataSource = new DynamicDataSource();
@@ -39,12 +42,13 @@ public class MybatisConfig {
     }
 
     @Bean
-    @ConfigurationProperties(prefix = "mybatis")
     public SqlSessionFactoryBean sqlSessionFactoryBean() throws IOException {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         PathMatchingResourcePatternResolver pathMatchingResourcePatternResolver = new PathMatchingResourcePatternResolver();
-        sqlSessionFactoryBean.setTypeAliasesPackage("com.example.**.domain");
-        sqlSessionFactoryBean.setMapperLocations(pathMatchingResourcePatternResolver.getResources("classpath*:mapping/*Mapper.xml"));
+        
+        sqlSessionFactoryBean.setTypeAliasesPackage(env.getProperty("mybatis.type-aliases-package"));
+        sqlSessionFactoryBean.setConfigLocation(pathMatchingResourcePatternResolver.getResource(env.getProperty("mybatis.config-location")));
+        sqlSessionFactoryBean.setMapperLocations(pathMatchingResourcePatternResolver.getResources(env.getProperty("mybatis.mapper-locations")));
         sqlSessionFactoryBean.setDataSource(dynamicDataSource());
         return sqlSessionFactoryBean;
     }
